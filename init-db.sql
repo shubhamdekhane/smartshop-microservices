baff1183f7e506fc8bbc041f44d8f911130e0c92
shubhamdekhane <115565872+shubhamdekhane@users.noreply.github.com>
2026-05-29 23:29:41 +0530

    fix docker issue
    fix docker issue

--- init-db.sql
+++ init-db.sql
@@ -1,4 +1,14 @@
+-- Create all databases
 CREATE DATABASE IF NOT EXISTS smartshop_user;
 CREATE DATABASE IF NOT EXISTS smartshop_product;
 CREATE DATABASE IF NOT EXISTS smartshop_order;
 CREATE DATABASE IF NOT EXISTS smartshop_payment;
+
+-- Give the 'smartshop' user full access to all databases
+GRANT ALL PRIVILEGES ON smartshop_user.* TO 'smartshop'@'%';
+GRANT ALL PRIVILEGES ON smartshop_product.* TO 'smartshop'@'%';
+GRANT ALL PRIVILEGES ON smartshop_order.* TO 'smartshop'@'%';
+GRANT ALL PRIVILEGES ON smartshop_payment.* TO 'smartshop'@'%';
+
+-- Apply the changes immediately
+FLUSH PRIVILEGES;
